97e670b324bc43b755a51eba110e23266fcddb73
spirit-revenge <www.2861684963@qq.com>
2025-10-15 10:36:22 +0900

    add:添加了GameModeBase和GameplayTag
    在项目中初始化了GameMode和创建了用于构建GameplayTag的C++文件

---
+++ Source/CombatProject/Private/CombatProjectGameplayTags.cpp
@@ -0,0 +1,11 @@
+// Spirit Revenge All Rights Reserved
+
+
+#include "CombatProjectGameplayTags.h"
+
+namespace CombatCombatProjectGameplayTags
+{
+	/** Input Tags **/
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look");
+}
---
+++ Source/CombatProject/Public/GameModes/CombatProjectBaseGameMode.h
@@ -0,0 +1,17 @@
+// Spirit Revenge All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CombatProjectBaseGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMBATPROJECT_API ACombatProjectBaseGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
